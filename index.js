bc778e733a94a01e9f94da78b51d5c185fed8c01
IK62 <ismail.karrouch13@gmail.com>
2022-10-19 14:42:22 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const btn = document.querySelectorAll('button');
 
+//load calculation
+let calculation = ("")
+
+//get every element in the table
 btn.forEach(function (btn) {
+    //detect the click
     btn.addEventListener('click', (e) => {
-        console.log(e.target.textContent);
-        number = e.target.textContent;
-        document.getElementById("output").innerHTML = output.textContent + number
-        
+        //calcul the result
+        if (e.target.textContent === "="){
+            document.getElementById("output").innerHTML = eval(calculation)
+            calculation = ("")
+        //reset the display
+        }else if (e.target.textContent === "AC"){
+            calculation = ("")
+            document.getElementById("output").innerHTML = "0"
+        //display calculation
+        }else{
+            //add the clicked element to calculation
+            calculation = calculation + e.target.textContent;
+            document.getElementById("output").innerHTML = calculation
+        }
     });
 });
